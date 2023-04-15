4867f84bbcf80f72cf52f5cf15d76179c4792308
Nam Hoang <hainam25101997@gmail.com>
2023-04-15 22:31:28 +0700

    fix

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { Routes, Route, Navigate, redirect } from "react-router-dom";
+
+import RtlLayout from "./layouts/rtl";
+import AdminLayout from "./layouts/admin";
+import AuthLayout from "./layouts/auth";
+const App = () => {
+  const [token, setToken] = useState();
+  if (0) {
+    return <AuthLayout setToken={setToken} />
+  }
+  return (
+    <Routes>
+      <Route path="auth/*" element={<AuthLayout />} />
+      <Route path="admin/*" element={<AdminLayout />} />
+      <Route path="rtl/*" element={<RtlLayout />} />
+      <Route path="/" element={<Navigate to="/admin" replace />} />
+    </Routes>
+  );
+};
+
+export default App;
